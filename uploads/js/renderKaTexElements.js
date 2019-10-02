05642f2b1c7f3bd70029f934165d4423276246ae
alex-cannon <alexcannon2018@gmail.com>
2019-10-02 13:01:38 -0600

    feat: katex ready fro production.

--- uploads/js/renderKaTexElements.js
+++ uploads/js/renderKaTexElements.js
@@ -1,6 +1,5 @@
 let renderKaTexElements = () => {
   [...document.getElementsByClassName("katex-expression")].map(kaTexElem => {
-    console.log(kaTexElem);
     katex.render(kaTexElem.innerHTML, kaTexElem, {
       throwOnError: false
     });
